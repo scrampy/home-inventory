34373290b05102fd311070d7722353e49ba35e2a
scrampy <scrampy@gmail.com>
2025-04-24 21:41:55 +0000

    Remove obsolete log, debug, and image files from project root.

--- query_milk.sql
+++
@@ -1,5 +0,0 @@
-SELECT s.id as store_id, s.name as store_name, m.id as item_id, m.name as item_name, sli.store_id
-FROM shopping_list_item sli
-JOIN master_item m ON sli.item_id = m.id
-LEFT JOIN store s ON sli.store_id = s.id
-WHERE m.name = 'Milk';
